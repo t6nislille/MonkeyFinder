37836835d5fba0f59f02d581f68688b868572dad
Tõnis <tonislille@gmail.com>
2023-06-08 02:09:55 +0300

    Monkey service

--- Part 1 - Displaying Data/MonkeyFinder/Services/MonkeyService.cs
+++ Part 1 - Displaying Data/MonkeyFinder/Services/MonkeyService.cs
@@ -1,5 +1,30 @@
-﻿namespace MonkeyFinder.Services;
+﻿using System.Net.Http.Json;
+
+namespace MonkeyFinder.Services;
 
 public class MonkeyService
 {
+    HttpClient httpClient;
+    List<Monkey> monkeyList = new();
+
+    public MonkeyService()
+    {
+        this.httpClient = new HttpClient();
+    }
+
+    public async Task<List<Monkey>> GetMonkeys()
+    {
+        if (monkeyList?.Count > 0)
+        {
+            return monkeyList;
+        }
+
+        var response = await httpClient.GetAsync("https://www.montemagno.com/monkeys.json");
+        if (response.IsSuccessStatusCode)
+        {
+            monkeyList = await response.Content.ReadFromJsonAsync<List<Monkey>>();
+        }
+
+        return monkeyList;
+    }
 }
