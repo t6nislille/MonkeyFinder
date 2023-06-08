24e823d5db164363b1ed142c97e5307d0ed291d4
Tõnis <tonislille@gmail.com>
2023-06-08 03:23:33 +0300

    Monkey location on map

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
@@ -1,8 +1,36 @@
-﻿namespace MonkeyFinder.ViewModel;
+﻿using Microsoft.Maui.ApplicationModel;
+
+namespace MonkeyFinder.ViewModel;
 
 [QueryProperty("Monkey", "Monkey")]
 public partial class MonkeyDetailsViewModel : BaseViewModel
 {
+    IMap map;
+
+    public MonkeyDetailsViewModel(IMap map)
+    {
+        this.map = map;
+    }
+
     [ObservableProperty]
     Monkey monkey;
+
+    [RelayCommand]
+    async Task OpenMap()
+    {
+        try
+        {
+            await map.OpenAsync(Monkey.Latitude, Monkey.Longitude, new MapLaunchOptions
+            {
+                Name = Monkey.Name,
+                NavigationMode = NavigationMode.None
+            });
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+            await Shell.Current.DisplayAlert("Error!",
+                $"Unable to open map: {ex.Message}", "OK");
+        }
+    }
 }
